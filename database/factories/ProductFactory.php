ec0a9e10fe15119468266b460bb5d2847e7a8714
TharathDev <tharath.dev@gmail.com>
2023-01-13 20:56:34 +0700

    init project

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->name(),
+            'price' => fake()->randomNumber(2, true),
+            'discound' => fake()->randomNumber(1, true),
+            'description' => fake()->sentence(5),
+        ];
+    }
+}
